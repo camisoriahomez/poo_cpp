dad52d8cf027128aa0fd81df0fbc1990e1ac572b
camisoriahomez <camilasoriahz@gmail.com>
2023-01-13 17:15:35 -0300

    ej bits listo

--- P1/ej5.cpp
+++ P1/ej5.cpp
@@ -8,32 +8,45 @@ using namespace std;
 
 //retorna la cantidad de bits en 1 presentes en value
 int numOnes(int value){
-  int ones[8];
-  int exp = 8;
-  int aux = value;
+  int cont = 0;
+  int exp = sizeof(value)*8;
   while (exp >= 0){
-    //cout << "DEBUG: valor=" << aux << " exp: " << pow(2,exp) << endl;
-    if (aux > pow(2, exp)){
-      cont++;
-      aux = aux % int(pow(2, exp));
-    }
+    if (value & int(pow(2,exp))) cont++;
     exp--;
   }
   return cont;
 }
 
 int setBit(int value, int bit){
-
+  int num = int(pow(2,bit));
+  return value | num;
 }
 
+int resetBit(int value, int bit){
+  int num = int(pow(2,bit));
+  return value & ~num;
+}
+
+bool testBit(int value, int bit){
+  int num = int(pow(2,bit));
+  return value & num;
+}
 
 int main(){
   int value, bit;
   cout << "Ingrese valor... " << endl;
   cin >> value;
-  cout << "Ingrese bit a setear en 1 (1-8): " << endl;
-  cin << bit;
+  cout << "Ingrese bit a setear en 1: " << endl;
+  cin >> bit;
 
-  int cantUnos = numOnes(value);
-  cout << "Cantidad de 1: " << cantUnos << endl;    
+  //int cantUnos = numOnes(value);
+  //cout << "Cantidad de 1: " << cantUnos << endl;   
+
+  int seteado = setBit(value, bit);
+  cout << "Numero original: " << value << ". Numero con bit seteado: " << seteado << endl; 
+
+  int reseteado = resetBit(value, bit);
+  cout << "Numero original: " << value << ". Numero con bit seteado: " << reseteado << endl;
+
+  cout << "Numero original: " << value << ". Status del bit: " << testBit(value, bit) << endl;
 }
